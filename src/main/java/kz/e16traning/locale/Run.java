d533d97d504199ef7dcd6f89bb61cfcd699276ee
MyWayToJavaJunior <hyperjava@mail.ru>
2015-08-10 22:49:04 +0600

    minor updates

--- src/main/java/kz/e16traning/locale/Run.java
+++ src/main/java/kz/e16traning/locale/Run.java
@@ -1,45 +1,41 @@
 package kz.e16traning.locale;
 
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 /**
- * Created by X1 on 10.08.2015.
+ * @author DK
+ * @version 1.0.0
+ *
  */
 public class Run {
+    private static ResourceBundle rb;
     public static void main(String[] args) throws UnsupportedEncodingException {
-        System.out.println("ru - 0 / en - 1");
-        char choiceOfLang = 0;
+        Scanner sc = new Scanner(System.in);
         try {
-            choiceOfLang = (char) System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("ru - 0 / en - 1");
+            String[] lang = {"RU", "RU"};
+            if ("1".equals(sc.nextLine())) {
+                lang[0] = "US";
+                lang[1] = "en";
+            }
+            Locale currentLocale = new Locale(lang[1], lang[0]);
+            rb = ResourceBundle.getBundle("property.text", currentLocale);
+            System.out.println("1: " + getPropByKey("menu1.question1"));
+            System.out.println("2: " + getPropByKey("menu1.question2"));
+            System.out.println("1 / 2");
+            if ("1".equals(sc.nextLine())) {
+                System.out.println(getPropByKey("menu1.answer1"));
+            } else {
+                System.out.println(getPropByKey("menu1.answer2"));
+            }
+        } finally {
+            sc.close();
         }
-        String[] lang = {"RU", "RU"};
-        if ("1".equals(choiceOfLang)) {
-            lang[0] = "US";
-            lang[1] = "EN";
-        }
-        Locale currentLocale = new Locale(lang[1], lang[0]);
-        ResourceBundle rb = ResourceBundle.getBundle("property.text", currentLocale);
-        String question1 = rb.getString("menu1.question1");
-        String rquestion1 = new String(question1.getBytes("ISO-8859-1"), "UTF-8");
-        String question2 = rb.getString("menu1.question1");
-        String rquestion2 = new String(question2.getBytes("ISO-8859-1"), "UTF-8");
-        System.out.println("1: " + rquestion1);
-        System.out.println("2: " + rquestion2);
-
-        try {
-            choiceOfLang = (char) System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        if ("1".equals(choiceOfLang)) {
-            System.out.println(rb.getString("menu1.answer1"));
-        } else {
-            System.out.println(rb.getString("menu1.answer2"));
-        }
+    }
+    public static String getPropByKey(String key) throws UnsupportedEncodingException {
+        return new String(rb.getString(key).getBytes("ISO-8859-1"), "UTF-8");
     }
 }
